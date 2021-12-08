8278f0457267248b8e2119988254b7bc92dd7b1a
Shannon Hicks <shannonehiccups@gmail.com>
2021-12-08 15:36:46 -0600

    worked on styling for the mood slider page

--- src/moodrange.js
+++ src/moodrange.js
@@ -1,19 +1,21 @@
 const MoodRange = () => {
     return ( 
-<div class="container container-fluid">
+<div class="container container-fluid moodFrame">
 <h1>How are you feeling today?</h1>
-<section class="row">
+<div class="container grid-container moodsAndSlider">
+<section class="form-container">
 <label for="customRange3" class="form-label"></label>
 <input type="range" class="form-range" min="0" max="100" step="20" id="customRange3"></input>
 </section>
-<section class="row"> 
-<p>Euphoric</p>
-<p>Happy</p>
-<p>Content</p>
-<p>Ennui</p>
-<p>Depressed</p>
-<p>Upset/Angry</p>
-</section>
+<div class="moodList">
+<p class="">Euphoric</p>
+<p class="">Happy</p>
+<p class="">Content</p>
+<p class="">Ennui</p>
+<p class="">Depressed</p>
+<p class="">Upset/Angry</p>
+</div>
+</div>
 </div>
      );
 }
