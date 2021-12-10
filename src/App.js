debcba4fd291b5f8e45f9f85dea222694d12f9b6
Shannon Hicks <88846622+shannonehiccups@users.noreply.github.com>
2021-12-10 19:09:48 +0000

    added functions from Kathy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import './App.css';
 import MoodRange from './moodrange';
+import MyMood from "./MyMood";
+import { useState, useEffect } from 'react';
 
 function App() {
   return (
@@ -9,5 +11,43 @@ function App() {
     </div>
   );
 }
+
+
+// Function from Kathy
+
+// const userEntry = [
+//   'anxious',
+//   'frustrated',
+//   'discouraged',
+//   'content',
+//   'optimistic',
+//   'energized'
+// ];
+
+
+// function App() {
+//   const [num, setNum] = useState(0);
+
+//   useEffect(() => {
+//     alert("Your mood has displayed");
+//   }, [num]);
+//   function newNum() {
+//     if (num < userEntry.length - 1) {
+//       setNum(num + 1);
+//     } else {
+//       setNum(0);
+//     }
+//   }
+
+//   return (
+//     <div>
+//       <h1>Mood Tracker</h1>
+//       <MyMood userEntry={newNum} />
+//       <h1>Your answer indicates that you feel {userEntry[num]}.</h1>
+//     </div>
+//   );
+// }
+
+
 
 export default App;
---
+++ src/myMood.js
@@ -0,0 +1,16 @@
+function MyMood({userEntry}){
+      
+  return ( 
+   
+   <div> 
+{/* userEntry needs to go in this sentence not the button */}
+    {/* <h1>Your choice indicates that you feel {moods}</h1> */}
+{/* Used button as test/placeholder */}
+{/* I think that the slider will replace the button in determining the num */}
+    <button onClick ={userEntry}>My Mood</button>
+    </div> 
+  );
+  } 
+export default MyMood;
+
+// from Kathy
